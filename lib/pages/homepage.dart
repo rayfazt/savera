7d5c07dd187becaf8aa2ce58a7e168078fd237c3
delishaandr <icha.delishaan@gmail.com>
2020-08-07 17:31:38 +0700

    add text on homepage and center panic button

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -11,18 +11,67 @@ class _HomeState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
-      floatingActionButton: Center(
-        child: RawMaterialButton(
-          onPressed: (){},
-          fillColor: Colors.red[400],
-          child: Icon(
-            Icons.warning,
-            size: 100.0,
-            color: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 130.0),
+          Text(
+            'Hai, Joe.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30.0,
+              color: Colors.white,
+            ),
           ),
-          padding: EdgeInsets.all(50.0),
-          shape: CircleBorder(),
-        ),
+          Text(
+            'Apakah kamu melihat atau mengalami kekerasan seksual?',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 120.0),
+          RawMaterialButton(
+            onPressed: (){},
+            fillColor: Colors.red[400],
+            child: Icon(
+              Icons.warning,
+              size: 100.0,
+              color: Colors.white,
+            ),
+            padding: EdgeInsets.all(50.0),
+            shape: CircleBorder(),
+          ),
+          SizedBox(height: 30.0),
+          Text(
+            'TEKAN TOMBOL DI ATAS UNTUK MENGAKTIFKAN PANIC BUTTON',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 22.0,
+              color: Colors.yellow,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 5.0),
+          Text(
+            'atau tekan tulisan ini jika kamu ingin membantu korban di sekitarmu',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 19.0,
+              color: Colors.grey,
+            ),
+          ),
+          SizedBox(height: 150.0),
+          Text(
+            'savera',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.grey[500],
+            ),
+          ),
+        ],
       ),
     );
   }
